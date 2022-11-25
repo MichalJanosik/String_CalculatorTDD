ac105a550731fe2bb79cd9d55bf4447b455d7c5f
MichalJanosik <janosik.michal.jm@gmail.com>
2022-11-25 14:19:09 +0100

    implement stringIsNumber other testcases

--- src/main/java/com/swissre/service/NumberValidatorService.java
+++ src/main/java/com/swissre/service/NumberValidatorService.java
@@ -1,7 +1,12 @@
 package com.swissre.service;
 
+import java.util.Objects;
+
 public class NumberValidatorService {
     public boolean stringIsNumber(String input) {
+        if (Objects.isNull(input)) {
+            return false;
+        }
         return input.matches("-?\\d+(\\.\\d+)?");
     }
 }
--- src/test/java/service/NumberValidatorServiceTest.java
+++ src/test/java/service/NumberValidatorServiceTest.java
@@ -3,6 +3,7 @@ package service;
 import com.swissre.service.NumberValidatorService;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumberValidatorServiceTest {
@@ -12,6 +13,27 @@ public class NumberValidatorServiceTest {
         String input = "1";
 
         assertTrue(new NumberValidatorService().stringIsNumber(input));
+    }
+
+    @Test
+    void stringIsNumber_NOT_NUMBER() {
+        String input = "a";
+
+        assertFalse(new NumberValidatorService().stringIsNumber(input));
+    }
+
+    @Test
+    void stringIsNumber_NULL() {
+        String input = null;
+
+        assertFalse(new NumberValidatorService().stringIsNumber(input));
+    }
+
+    @Test
+    void stringIsNumber_EMPTY() {
+        String input = "";
+
+        assertFalse(new NumberValidatorService().stringIsNumber(input));
     }
 
 
