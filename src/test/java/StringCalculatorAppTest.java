0afd4f115f64434965e0b3eef66b4aeb834c7400
MichalJanosik <janosik.michal.jm@gmail.com>
2022-11-25 12:07:32 +0100

    refactor null_empty_return_zero

--- src/test/java/StringCalculatorAppTest.java
+++ src/test/java/StringCalculatorAppTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorAppTest {
 
     @Test
-    void inputString_EMPTY_RETURN_ZERO() {
+    void addString_EMPTY_RETURN_ZERO() {
         assertEquals(0, StringCalculatorApp.addString(""));
     }
 
     @Test
-    void inputString_NULL_RETURN_ZERO() {
+    void addString_NULL_RETURN_ZERO() {
         assertEquals(0, StringCalculatorApp.addString(null));
     }
 }
