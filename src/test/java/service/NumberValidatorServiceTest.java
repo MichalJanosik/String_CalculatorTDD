46f2bc4e8855d4d0cab428dc6779da8ae056ee7a
MichalJanosik <janosik.michal.jm@gmail.com>
2022-11-25 14:39:15 +0100

    refactor NumberValidatorServiceTest.java

--- src/test/java/service/NumberValidatorServiceTest.java
+++ src/test/java/service/NumberValidatorServiceTest.java
@@ -2,7 +2,6 @@ package service;
 
 import com.swissre.service.NumberValidatorService;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -10,52 +9,59 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumberValidatorServiceTest {
 
+    static NumberValidatorService numberValidator;
+
+    @BeforeAll
+    static void initialSetup() {
+        numberValidator = new NumberValidatorService();
+    }
+
     @Test
     void stringIsNumber_OK() {
         String input = "1";
 
-        assertTrue(new NumberValidatorService().stringIsNumber(input));
+        assertTrue(numberValidator.stringIsNumber(input));
     }
 
     @Test
     void stringIsNumber_NOT_NUMBER() {
         String input = "a";
 
-        assertFalse(new NumberValidatorService().stringIsNumber(input));
+        assertFalse(numberValidator.stringIsNumber(input));
     }
 
     @Test
     void stringIsNumber_NULL() {
         String input = null;
 
-        assertFalse(new NumberValidatorService().stringIsNumber(input));
+        assertFalse(numberValidator.stringIsNumber(input));
     }
 
     @Test
     void stringIsNumber_EMPTY() {
         String input = "";
 
-        assertFalse(new NumberValidatorService().stringIsNumber(input));
+        assertFalse(numberValidator.stringIsNumber(input));
     }
 
     @Test
     void numberIsValid_OK() {
         int number = 1;
 
-        assertTrue(new NumberValidatorService().numberIsValid(number));
+        assertTrue(numberValidator.numberIsValid(number));
     }
 
     @Test
     void numberIsValid_NEGATIVE_NUMBER() {
         int number = -1;
 
-        assertFalse(new NumberValidatorService().numberIsValid(number));
+        assertFalse(numberValidator.numberIsValid(number));
     }
 
     @Test
     void numberIsValid_GREATER_THAN_100() {
         int number = 101;
 
-        assertFalse(new NumberValidatorService().numberIsValid(number));
+        assertFalse(numberValidator.numberIsValid(number));
     }
 }
